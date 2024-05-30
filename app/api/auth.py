1afda38952d194c09e42953047245fd8bfc873b3
leo_liu <leo_liu@raytrex.com>
2024-05-30 10:47:27 +0800

    change respone message -> msg

--- app/api/auth.py
+++ app/api/auth.py
@@ -22,44 +22,47 @@ def check(fn):
         try:
             verify_jwt_in_request()
         except ExpiredSignatureError as e:
-            return jsonify({'msg': 'access expired'}), 401
+            return jsonify({"msg": "access expired"}), 401
         except Exception as e:
             print(e)
-            return jsonify({'msg': 'Internal server error'}), 500
+            return jsonify({"msg": "Internal server error"}), 500
 
         return fn(*args, **kwargs)
+
     return decorator
 
 
-@api.route('/verify-token', methods=["GET"])
+@api.route("/verify-token", methods=["GET"])
 @check
 def verify():
-    return jsonify({'msg': 'Token is valid'}), 200
+    return jsonify({"msg": "Token is valid"}), 200
 
 
-@api.route('/login', methods=["POST"])
+@api.route("/login", methods=["POST"])
 def login():
-    col = mongo_client.client['db_create_by_leo']['collection_create_by_leo']
+    col = mongo_client.client["db_create_by_leo"]["collection_create_by_leo"]
 
-    email = request.json.get('email')
-    password = request.json.get('password')
+    email = request.json.get("email")
+    password = request.json.get("password")
 
-    dbquery = {
-        "email": email,
-        'password': password
-    }
+    dbquery = {"email": email, "password": password}
 
     mydoc = list(col.find(dbquery))
-
+    print("///////////////////", mydoc)
     if len(mydoc) == 0:
         return {"msg": "Wrong email or password"}, 401
 
-    response = jsonify({"msg": "successful", "data": {
-        'id': str(mydoc[0]['_id']),
-        'username': mydoc[0]['username'],
-        'email': mydoc[0]['email'],
-        'favorite': mydoc[0]['favorite']
-    }})
+    response = jsonify(
+        {
+            "msg": "successful",
+            "data": {
+                "id": str(mydoc[0]["_id"]),
+                "username": mydoc[0]["username"],
+                "email": mydoc[0]["email"],
+                "favorite": mydoc[0]["favorite"],
+            },
+        }
+    )
 
     access_token = create_access_token(identity=email)
     refresh_token = create_refresh_token(identity=email)
@@ -70,42 +73,36 @@ def login():
     return response
 
 
-@api.route('/signup', methods=["POST"])
+@api.route("/signup", methods=["POST"])
 def signup():
-    col = mongo_client.client['db_create_by_leo']['collection_create_by_leo']
+    col = mongo_client.client["db_create_by_leo"]["collection_create_by_leo"]
 
-    username = request.json.get('username')
-    email = request.json.get('email')
-    password = request.json.get('password')
+    username = request.json.get("username")
+    email = request.json.get("email")
+    password = request.json.get("password")
 
-    dbquery = {
-        "username": username,
-        'password': password
-    }
+    dbquery = {"username": username, "password": password}
 
     mydoc = col.find(dbquery)
 
     if len(list(mydoc)) != 0:
-        return jsonify({'msg': 'user exist'}), 409
+        return jsonify({"msg": "user exist"}), 409
 
-    col.insert_one({
-        "username": username,
-        "email": email,
-        "password": password,
-        'favorite': []
-    })
+    col.insert_one(
+        {"username": username, "email": email, "password": password, "favorite": []}
+    )
 
-    return jsonify({'message': 'success'}), 201
+    return jsonify({"message": "success"}), 201
 
 
-@api.route('/logout', methods=["POST"])
+@api.route("/logout", methods=["POST"])
 def logout():
     response = jsonify({"msg": "logout successful"})
     unset_jwt_cookies(response)
     return response
 
 
-@api.route('/refresh', methods=['POST'])
+@api.route("/refresh", methods=["POST"])
 @jwt_required(refresh=True)
 def refresh():
     # Create the new access token
@@ -113,51 +110,49 @@ def refresh():
     access_token = create_access_token(identity=current_user)
 
     # Set the JWT access cookie in the response
-    resp = jsonify({'refresh': True})
+    resp = jsonify({"refresh": True})
     set_access_cookies(resp, access_token)
     return resp, 200
 
 
-@api.route('/addFavorite', methods=['POST'])
+@api.route("/addFavorite", methods=["POST"])
 @check
 def get_favorite():
-    col = mongo_client.client['db_create_by_leo']['collection_create_by_leo']
+    col = mongo_client.client["db_create_by_leo"]["collection_create_by_leo"]
 
     current_user_email = get_jwt_identity()
-    movieID = request.json.get('movieID')
+    movieID = request.json.get("movieID")
 
-    existing_favorite = col.find_one({"email": current_user_email, "favorite": {"$in": [movieID]}})
-
-    if existing_favorite:
-        return jsonify({"message": "Favorite already exists"}), 400
-
-    col.update_one(
-        {"email": current_user_email},
-        {"$push": {"favorite": movieID}}
+    existing_favorite = col.find_one(
+        {"email": current_user_email, "favorite": {"$in": [movieID]}}
     )
 
-    favoriteMovies = col.find_one({"email": current_user_email})['favorite']
+    if existing_favorite:
+        return jsonify({"msg": "Favorite already exists"}), 400
+
+    col.update_one({"email": current_user_email}, {"$push": {"favorite": movieID}})
+
+    favoriteMovies = col.find_one({"email": current_user_email})["favorite"]
 
     response = jsonify({"msg": "successful", "data": favoriteMovies})
 
     return response, 200
 
 
-@api.route('/removeFavorite', methods=['DELETE'])
+@api.route("/removeFavorite", methods=["DELETE"])
 @check
 def rm_favorite():
     try:
-        col = mongo_client.client['db_create_by_leo']['collection_create_by_leo']
+        col = mongo_client.client["db_create_by_leo"]["collection_create_by_leo"]
 
-        movieID = request.json.get('movieID')
+        movieID = request.json.get("movieID")
         current_user_email = get_jwt_identity()
 
         result = col.update_one(
-            {"email": current_user_email},
-            {"$pull": {"favorite": movieID}}
+            {"email": current_user_email}, {"$pull": {"favorite": movieID}}
         )
 
-        favoriteMovies = col.find_one({"email": current_user_email})['favorite']
+        favoriteMovies = col.find_one({"email": current_user_email})["favorite"]
         if result.modified_count == 1:
             return jsonify({"msg": "success", "data": favoriteMovies}), 200
         else:

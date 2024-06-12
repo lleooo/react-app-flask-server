1a9cfea4d4e5afa55b16401f1bc8ed2423ed061f
leo_liu <leo_liu@raytrex.com>
2024-06-12 12:09:09 +0800

    add google sign in api

--- app/api/auth.py
+++ app/api/auth.py
@@ -15,6 +15,11 @@ from flask_jwt_extended import get_jwt_identity
 from jwt.exceptions import ExpiredSignatureError
 from jwt import decode
 
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
+GOOGLE_AUTH_CLINT_ID = "54532982628-dmk3e53gfh1djcel7htm5pkiq3h85u9g.apps.googleusercontent.com"
+
 
 def check(fn):
     @wraps(fn)
@@ -24,7 +29,6 @@ def check(fn):
         except ExpiredSignatureError as e:
             return jsonify({"msg": "access expired"}), 401
         except Exception as e:
-            print(e)
             return jsonify({"msg": "Internal server error"}), 500
 
         return fn(*args, **kwargs)
@@ -48,7 +52,7 @@ def login():
     dbquery = {"email": email, "password": password}
 
     mydoc = list(col.find(dbquery))
-    print("///////////////////", mydoc)
+
     if len(mydoc) == 0:
         return {"msg": "Wrong email or password"}, 401
 
@@ -115,8 +119,58 @@ def refresh():
     return resp, 200
 
 
-@api.route("/addFavorite", methods=["POST"])
-@check
+@api.route("/googleSignIn", methods=["POST"])
+def google_sign_in():
+    col = mongo_client.client["db_create_by_leo"]["collection_create_by_leo"]
+    token = request.json['credential']
+
+    # verify token first
+    try:
+        id_info = id_token.verify_oauth2_token(
+            token,
+            requests.Request(),
+            GOOGLE_AUTH_CLINT_ID
+        )
+
+        if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            return jsonify({"error": "Wrong issuer"}), 400
+    except ValueError as e:
+        return jsonify({"error": "Invalid token"}), 400
+
+    user = col.find_one({"username": id_info['name'], "email": id_info['email']})
+
+    if not user:
+        user_data = {
+            "username": id_info['name'],
+            "email": id_info['email'],
+            "password": '',
+            "favorite": []
+        }
+        col.insert_one(user_data)
+
+    else:
+        user_data = user
+
+    response_data = jsonify({
+        "msg": "successful",
+        "data": {
+            "username": user_data["username"],
+            "email": user_data["email"],
+            "favorite": user_data["favorite"]
+        }
+    })
+
+    access_token = create_access_token(identity=id_info['email'])
+    refresh_token = create_refresh_token(identity=id_info['email'])
+
+    set_access_cookies(response_data, access_token)
+    set_refresh_cookies(response_data, refresh_token)
+
+    return response_data, 200
+
+
+@ api.route("/addFavorite", methods=["POST"])
+@ check
 def get_favorite():
     col = mongo_client.client["db_create_by_leo"]["collection_create_by_leo"]
 
@@ -139,8 +193,8 @@ def get_favorite():
     return response, 200
 
 
-@api.route("/removeFavorite", methods=["DELETE"])
-@check
+@ api.route("/removeFavorite", methods=["DELETE"])
+@ check
 def rm_favorite():
     try:
         col = mongo_client.client["db_create_by_leo"]["collection_create_by_leo"]

fafc345b37941a419d35e03448148671240b29f8
Leo <leo88728@gmail.com>
2025-12-11 18:15:43 +0800

    add certifi

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,11 +1,15 @@
 from pymongo import MongoClient
+import certifi
 
 class MongoDBClient:
     def __init__(self):
         self.client = None
 
     def init_app(self, app):
-        self.client = MongoClient(app.config['MONGO_URI'])
+        self.client = MongoClient(
+            app.config['MONGO_URI'],
+            tlsCAFile=certifi.where()
+        )
         print(self.client)
 
 mongo_client = MongoDBClient()
